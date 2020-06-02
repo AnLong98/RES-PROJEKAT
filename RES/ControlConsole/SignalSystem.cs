5bbeeb89ef596a79ac8d20e93129426c13fc72ff
Predrag <svetoizvorac@yahoo.com>
2020-06-02 23:13:22 +0200

    Added reader to this beyatch. Time to go sleeping.

--- RES/ControlConsole/SignalSystem.cs
+++ RES/ControlConsole/SignalSystem.cs
@@ -3,6 +3,7 @@ using InputNS;
 using LoggerSpace;
 using Modul1;
 using Module2;
+using ReaderNS;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +15,11 @@ namespace ControlConsole
     public class SignalSystem
     {
         private IInput inputProxy;
+        private IReader readingProxy;
         private static SignalSystem singletonSystemInstance;
 
         public IInput InputProxy { get => inputProxy; }
+        public IReader ReadingProxy { get => readingProxy;}
 
         private SignalSystem()
         {
@@ -29,12 +32,15 @@ namespace ControlConsole
             ILogging module1Logger = new Logger("module1_log.txt");
             ILogging module2Logger = new Logger("module2_log.txt");
             ILogging inputLogger = new Logger("input_log.txt");
+            ILogging readerLogger = new Logger("reader_log.txt");
 
             Module2ServiceProvider module2 = new Module2ServiceProvider(module2Logger);
             Module1ServiceProvider module1 = new Module1ServiceProvider(module1Logger, module2);
             Input input = new Input(inputLogger, module2, module1);
+            Reader reader = new Reader(readerLogger, module2);
             input.StartDataFlow();
             inputProxy = input;
+            readingProxy = reader;
         }
 
 
@@ -65,6 +71,43 @@ namespace ControlConsole
                 Console.WriteLine(e.Message);
             }
         }
+
+        private void InteractForHistoryReading()
+        {
+            WriteSignalMenuReading();
+            int signalCode = GetSignalCode();
+            if (signalCode == -1) return;
+            WriteBeginDateMenu();
+            string beginDate = GetDateValue();
+            WriteEndDateMenu();
+            string endDate = GetDateValue();
+            try
+            {
+                string allValues = ReadingProxy.ReadFromHistory(beginDate, endDate, signalCode);
+                Console.WriteLine("Following values exist within given timeframe:\n" + allValues);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private string GetDateValue()
+        {
+            while (true)
+            {
+                try
+                {
+                    string date = Console.ReadLine();
+                    return date;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error occured reading input.");
+                }
+            }
+        }
+
 
         private int GetSignalCode()
         {
@@ -116,12 +159,43 @@ namespace ControlConsole
             }
         }
 
+
+        private void WriteBeginDateMenu()
+        {
+            Console.WriteLine("---READING FROM HISTORY---");
+            Console.WriteLine("What is the starting date for the timeframe you wish to read?");
+            Console.WriteLine("Value should be a date in (dd-MM-yyy hh:mm:ss) format.");
+        }
+
+        private void WriteEndDateMenu()
+        {
+            Console.WriteLine("---READING FROM HISTORY---");
+            Console.WriteLine("What is the ending date for the timeframe you wish to read?");
+            Console.WriteLine("Value should be a date in (dd-MM-yyy hh:mm:ss) format.");
+        }
+
         private void WriteMainMenu()
         {
             Console.WriteLine("---MENU---");
             Console.WriteLine("1 - Read data with timeframe");
             Console.WriteLine("2 - Write directly to history");
             Console.WriteLine("0 - Exit");
+        }
+
+
+        private void WriteSignalMenuReading()
+        {
+            Console.WriteLine("---READING FROM HISTORY---");
+            Console.WriteLine("What signal do you wish to read?");
+            Console.WriteLine("0 - Analog");
+            Console.WriteLine("1 - Digital");
+            Console.WriteLine("2 - Custom");
+            Console.WriteLine("3 - Limitset");
+            Console.WriteLine("4 - Singlenode");
+            Console.WriteLine("5 - Multiplenode");
+            Console.WriteLine("6 - Consumer");
+            Console.WriteLine("7 - Source");
+            Console.WriteLine("-1 - Exit");
         }
 
 
@@ -158,7 +232,7 @@ namespace ControlConsole
                 switch (command)
                 {
                     case 1:
-                        Console.WriteLine("Not implemented yet");
+                        InteractForHistoryReading();
                         break;
 
                     case 2:
