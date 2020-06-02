83f5911f332f110d6ee0f259c69049a557fc2c39
Stefan Besovic <stefanbesovic@gmail.com>
2020-06-02 11:57:17 +0200

    Created reader without implementation

---
+++ RES/Reader/Reader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reader
+{
+    public class Reader
+    {
+        private ILogging logger;
+        private IModule2History module2proxy;
+
+        public Reader()
+        {
+
+        }
+
+        ~Reader()
+        {
+
+        }
+
+        public Reader(ILogging logger, IModule2History modul2proxy)
+        {
+            this.logger = logger;
+            this.module2proxy = modul2proxy;
+        }
+
+        public void ActivateReading()
+        {
+
+        }
+
+        public void ConnectToModul2()
+        {
+
+        }
+
+        public void Shutdown()
+        {
+
+        }
+
+
+    }
+}
